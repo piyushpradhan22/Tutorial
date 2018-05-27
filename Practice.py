09d9fc3d93ae23b43056b74648c856b71bc78fca
Python <piyushpradhan22@gmail.com>
2018-05-27 17:48:52 +0530

    initial commit

--- Practice.py
+++ Practice.py
@@ -1,4 +1,5 @@
 # Armstrong Number
+print('-------------Armstrong Number-----------')
 x =input('Input:')
 l=len(x)
 x=int(x)
@@ -12,3 +13,11 @@ if(s==x1):
     print('Output: Yes')
 else:
     print('Output: No')
+# Scope of variable
+print('-------------Scope of variable---------------')
+glb = 22
+def dd():
+    global glb
+    glb=glb+22
+dd()
+print(glb)
